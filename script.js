06a07e86fee317102fafccd09e1dd215fcaecf98
Jean Ngassa <96141846+pra-tek@users.noreply.github.com>
2023-09-04 17:35:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  // Reading time
+  function readingTime(){
+    const text = document.getElementById("article").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time").innerText = "~ " + time + "minutes";
+  }
+  readingTime();
